9bfae2d12c5bd9e6a2b5ecec6b398bc29510e9a0
Vihaan Krishnan <krisvih32@yahoo.com>
2025-07-04 06:21:36 -0700

    Asserts if code not within 0-255

--- src/file_handling.rs
+++ src/file_handling.rs
@@ -5,7 +5,7 @@ pub fn get_code() -> String {
         2 => fs::read_to_string(args[1].clone()).expect("Error: Unable to read file or directory"),
         // TODO: If release, uncomment this
         // _ => panic!("Wrong number of arguments, only pass file"),
-        _ => fs::read_to_string(String::from("tests/hello_world.nimra").clone())
+        _ => fs::read_to_string(String::from("tests/exit.nimra").clone())
             .expect("Error: Unable to read file or directory"),
     }
 }
