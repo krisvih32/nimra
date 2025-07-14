1dcc2df74188688832e23f6ebf14fbd5a702a323
Vihaan Krishnan <krisvih32@yahoo.com>
2025-07-13 18:41:53 -0700

    Adding error message

--- src/file_handling.rs
+++ src/file_handling.rs
@@ -5,7 +5,6 @@ pub fn get_code() -> Result<String, String> {
     match args.len() {
         2 => fs::read_to_string(args[1].clone())
             .map_err(|e| format!("Error: Unable to read file or directory: {e}")),
-        _ => fs::read_to_string(String::from("tests/exit.nimra"))
-            .map_err(|e| format!("Error: Unable to read file or directory: {e}")),
+        _ => Err("Error: Please provide exactly one argument (the file path)".to_string()),
     }
 }
